9cd4811a87a02f606b2ffdad61f59dfc5518a37d
Aditya Taksande <taksandeaditya@gmail.com>
2020-08-26 19:25:03 +0530

    Updated models,database,templates,database,views

--- store/views.py
+++ store/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def store(request):
-
-    context ={}
-    return render(request,'store/store.html')
+    products = Product.objects.all()
+    context ={'products':products}
+    return render(request,'store/store.html',context)
 
 def cart(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer)
     context ={}
     return render(request,'store/cart.html')
 
