8b8f529ded6a9bc50091989aff7d733458074a4b
Aditya Taksande <taksandeaditya@gmail.com>
2020-09-04 18:09:40 +0530

    view to templates ( cart count )

--- store/models.py
+++ store/models.py
@@ -58,6 +58,11 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return str(self.product)
+
 
     @property
     def get_total(self):
