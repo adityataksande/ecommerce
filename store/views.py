0b9889220346ebcb49e613589d00b166606e0002
Aditya Taksande <taksandeaditya@gmail.com>
2020-09-26 18:43:10 +0530

    Updated db, views, template

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 # Create your views here.
 def store(request):
@@ -82,4 +83,27 @@ def updateItem(request):
 
 
 def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id 
+
+        if total== order.get_cart_total:
+            order.complete = True 
+        order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            pincode = data['shipping']['pincode'],    
+        )
+    else:
+        print("User is not Logged in...")
     return JsonResponse('Payment Submitted..', safe=False)
